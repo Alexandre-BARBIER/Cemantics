837d2db0693d8fdd37391fe87a705249e350fbfb
Alexandre BARBIER <alexandre.barbier@student-cs.fr>
2023-10-18 18:47:43 +0200

    test

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import seedrandom from "npm:seedrandom";
 function getRandomInt(min, max) {
     
     var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
+    var dd = String(today.getDate()+1).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
     today = mm + '-' + dd + '-' + yyyy + '11';
