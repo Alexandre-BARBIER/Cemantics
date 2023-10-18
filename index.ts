c2a08bf92adc21c3ad22f85b1cfedbe50cef30e9
Alexandre BARBIER <alexandre.barbier@student-cs.fr>
2023-10-18 18:46:16 +0200

    test

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ function getRandomInt(min, max) {
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
-    today = mm + '-' + dd + '-' + yyyy;
+    today = mm + '-' + dd + '-' + yyyy + '11';
     console.log(today)
     const generator = seedrandom('today');
 
